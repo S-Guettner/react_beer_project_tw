9981a1081a76217de9f8f0bb42de237aa98f24f8
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-02-28 15:21:21 +0100

    styling

--- src/components/DetailviewConstructor.jsx
+++ src/components/DetailviewConstructor.jsx
@@ -2,13 +2,19 @@
 const DatailviewConstructor = (props) => {
     
     return ( 
-        <article>
-            <img className="w-40" src={props.img} alt="Beer" />
-            <h2>{props.name}</h2>
-            <p>{props.tagline}</p>
+        <article className="p-4 text-center">
+            <img className="w-40 mx-auto mb-10" src={props.img} alt="Beer" />
+            <h2 className="text-2xl mb-4">{props.name}</h2>
+            <p className="text-[#ffca41] mb-2">{props.tagline}</p>
+            <div className="flex justify-between w-52 mx-auto">
+                <p>First Brewed: </p>
             <p>{props.firstBrewed}</p>
+            </div>
+            <div className="flex justify-between w-52 mx-auto mb-4">
+            <p>Attenuation level:</p>
             <p>{props.attenuationLevel}</p>
-            <p>{props.description}</p>
+            </div>
+            <p className="w-2/3 mx-auto">{props.description}</p>
             <p>{props.contributedBy}</p>
         </article>
      );
