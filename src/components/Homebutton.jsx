204ca75cd4f5e9cfe827f5a1e697cee71efc155c
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-02-28 15:07:37 +0100

    styling

--- src/components/Homebutton.jsx
+++ src/components/Homebutton.jsx
@@ -3,7 +3,7 @@ import logoButton from '../assets/images/Logo.png'
 
 const HomeButton = () => {
     return ( 
-        <Link className="flex justify-center p-2 h-14 bg-[#ffca41]" to={"/"}>
+        <Link className="mt-20 flex justify-center p-2 h-14 bg-[#ffca41]" to={"/"}>
             <img src={logoButton} alt="" />
         </Link>
      );
--- src/components/OverviewConstructor.jsx
+++ src/components/OverviewConstructor.jsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom";
 const OverviewConstructor = (props) => {
     const productId = props.id
     return ( 
-        <article className="flex justify-center items-center p-3">
+        <article className="flex justify-center items-center w-96 mx-auto border-b-[.1rem] border-black">
             <img className="w-20 m-4" src={props.img} alt="Beer" />
-            <div className="text-center m-4 w-64">
+            <div className="text-center m-4 w-64 border-">
                 <h2>{props.name}</h2>
                 <p>{props.tagline}</p>
                 <p>Created by: {(props.contributedBy).slice(0,10)}</p>
