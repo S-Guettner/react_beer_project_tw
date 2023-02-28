1e44f4ebc5cdfd020614935adb3de872ab2f6263
S-Guettner-Dev <sven_guettner@protonmail.com>
2023-02-28 15:12:13 +0100

    styling

--- src/components/OverviewConstructor.jsx
+++ src/components/OverviewConstructor.jsx
@@ -6,10 +6,10 @@ const OverviewConstructor = (props) => {
         <article className="flex justify-center items-center w-96 mx-auto border-b-[.1rem] border-black">
             <img className="w-20 m-4" src={props.img} alt="Beer" />
             <div className="text-center m-4 w-64 border-">
-                <h2>{props.name}</h2>
-                <p>{props.tagline}</p>
-                <p>Created by: {(props.contributedBy).slice(0,10)}</p>
-                <Link className="h-10 w-20 inline-block bg-cyan-800" to={`/productdetails/${productId}`}>Details</Link>
+                <h2 className="text-2xl mb-2">{props.name}</h2>
+                <p className="text-[#ffca41] mb-1">{props.tagline}</p>
+                <p className="mb-4">Created by: {(props.contributedBy).slice(0,10)}</p>
+                <Link className="rounded-xl h-10 pt-2 w-20 inline-block bg-[#ffca41]" to={`/productdetails/${productId}`}>Details</Link>
             </div>
         </article>
      );
